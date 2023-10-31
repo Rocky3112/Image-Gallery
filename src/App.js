ee500bf8a1e40b121aba0bd79ee7f190c07a2796
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-11-01 00:57:37 +0600

    fixed some issue

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, { useState } from 'react';
-import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckSquare } from "@fortawesome/free-solid-svg-icons";
 
-import image1 from './images/image-1.webp';
-import image2 from './images/image-2.webp';
-import image3 from './images/image-3.webp';
-import image4 from './images/image-4.webp';
-import image5 from './images/image-5.webp';
-import image6 from './images/image-6.webp';
-import image7 from './images/image-7.webp';
-import image8 from './images/image-8.webp';
-import image9 from './images/image-9.webp';
-import image10 from './images/image-10.jpeg';
-import image11 from './images/image-11.jpeg';
+import image1 from "./images/image-1.webp";
+import image2 from "./images/image-2.webp";
+import image3 from "./images/image-3.webp";
+import image4 from "./images/image-4.webp";
+import image5 from "./images/image-5.webp";
+import image6 from "./images/image-6.webp";
+import image7 from "./images/image-7.webp";
+import image8 from "./images/image-8.webp";
+import image9 from "./images/image-9.webp";
+import image10 from "./images/image-10.jpeg";
+import image11 from "./images/image-11.jpeg";
 
 const initialImages = [
-  { id: '1', url: image1, isFeatured: false },
-  { id: '2', url: image2, isFeatured: false },
-  { id: '3', url: image3, isFeatured: false },
-  { id: '4', url: image4, isFeatured: false },
-  { id: '5', url: image5, isFeatured: false },
-  { id: '6', url: image6, isFeatured: false },
-  { id: '7', url: image7, isFeatured: false },
-  { id: '8', url: image8, isFeatured: false },
-  { id: '9', url: image9, isFeatured: false },
-  { id: '10', url: image10, isFeatured: false },
-  { id: '11', url: image11, isFeatured: false },
-  
+  { id: "1", url: image1, isFeatured: false },
+  { id: "2", url: image2, isFeatured: false },
+  { id: "3", url: image3, isFeatured: false },
+  { id: "4", url: image4, isFeatured: false },
+  { id: "5", url: image5, isFeatured: false },
+  { id: "6", url: image6, isFeatured: false },
+  { id: "7", url: image7, isFeatured: false },
+  { id: "8", url: image8, isFeatured: false },
+  { id: "9", url: image9, isFeatured: false },
+  { id: "10", url: image10, isFeatured: false },
+  { id: "11", url: image11, isFeatured: false },
 ];
 
 function App() {
@@ -70,7 +69,6 @@ function App() {
   };
   const selectedImageCount = images.filter((image) => image.isSelected).length;
 
-
   // const setFeatureImage = (id) => {
   //   const updatedImages = images.map((image) => ({
   //     ...image,
@@ -80,38 +78,54 @@ function App() {
   // };
 
   return (
-    <div className="App">
+    <div className="App p-10">
       <h1>Image Gallery</h1>
-    <h2><span className='text-blue-600'> <FontAwesomeIcon icon= {faCheckSquare} /></span> {selectedImageCount} Files Selected</h2>
-      <div className="actions">
-        <button className='btn bg-green-400 hover:bg-red-700 px-2 py-1 rounded-lg' onClick={deleteSelectedImages}>Delete Selected</button>
-        {/* <button onClick={() => setFeatureImage(images[0].id)}>Set as Feature</button> */}
+      <div className=" flex justify-between items-center py-5 px-16">
+        <h2>
+          <span className="text-blue-600">
+            {" "}
+            <FontAwesomeIcon icon={faCheckSquare} />
+          </span>{" "}
+          {selectedImageCount} Files Selected
+        </h2>
+        <div className="actions">
+          <button
+            className="btn bg-green-400 hover:bg-red-700 px-2 py-1 rounded-lg"
+            onClick={deleteSelectedImages}
+          >
+            Delete Selected
+          </button>
+          {/* <button onClick={() => setFeatureImage(images[0].id)}>Set as Feature</button> */}
+        </div>
       </div>
-      <div className="image-gallery grid grid-cols-4 gap-5 h-[700px] gap-y-10 ">
+      <div className="image-gallery grid lg:grid-cols-4 gap-5  gap-y-10 ">
         {images.map((image) => (
           <div
             key={image.id}
-            className={`image-item ${image.isFeatured ? 'featured' : ''} ${
-              image.isSelected ? 'selected' : ''
-            }  shadow-2xl h-[300px] w-[300px] rounded-lg`}
+            className={`image-item ${image.isFeatured ? "featured" : ""} ${
+              image.isSelected ? "selected" : ""
+            }  shadow-2xl h-[300px] w-[300px] rounded-lg text-center`}
             draggable
             onDragStart={(e) => handleDragStart(e, image.id)}
             onDragEnter={(e) => handleDragEnter(e, image.id)}
           >
             <input
-            className=''
+              className=""
               type="checkbox"
               checked={image.isSelected}
               onChange={() => toggleImageSelection(image.id)}
             />
-            <div className=''>
-              <img className='h-[240px]' src={image.url} alt={`Image ${image.id}`} />
+            <div className="flex justify-center items-center">
+              <img
+                className="h-[240px] rounded-lg"
+                src={image.url}
+                alt={`Image ${image.id}`}
+              />
             </div>
             {image.isFeatured && <div className="featured-label">Featured</div>}
           </div>
         ))}
       </div>
-      
     </div>
   );
 }
