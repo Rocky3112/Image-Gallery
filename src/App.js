b24e54efab90777fa0c4451ab8f3f449f344b444
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-10-31 22:56:05 +0600

    added all images

--- src/App.js
+++ src/App.js
@@ -4,11 +4,28 @@ import './App.css';
 import image1 from './images/image-1.webp';
 import image2 from './images/image-2.webp';
 import image3 from './images/image-3.webp';
+import image4 from './images/image-4.webp';
+import image5 from './images/image-5.webp';
+import image6 from './images/image-6.webp';
+import image7 from './images/image-7.webp';
+import image8 from './images/image-8.webp';
+import image9 from './images/image-9.webp';
+import image10 from './images/image-10.jpeg';
+import image11 from './images/image-11.jpeg';
 
 const initialImages = [
   { id: '1', url: image1, isFeatured: false },
   { id: '2', url: image2, isFeatured: false },
   { id: '3', url: image3, isFeatured: false },
+  { id: '4', url: image4, isFeatured: false },
+  { id: '5', url: image5, isFeatured: false },
+  { id: '6', url: image6, isFeatured: false },
+  { id: '7', url: image7, isFeatured: false },
+  { id: '8', url: image8, isFeatured: false },
+  { id: '9', url: image9, isFeatured: false },
+  { id: '10', url: image10, isFeatured: false },
+  { id: '11', url: image11, isFeatured: false },
+  
 ];
 
 function App() {
